67bce9f1421799055d3f6b9f83ad7a572aa69c3d
FengYi Lee <FengYi.li@hotmail.com>
2016-04-25 01:20:47 +0800

    ADD AudioManager and AudioRecorderButton java file

---
+++ app/src/main/java/com/yuan/skeleton/ui/view/AudioRecorderButton.java
@@ -0,0 +1,23 @@
+package com.yuan.skeleton.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by KevinLee on 2016/4/25.
+ */
+public class AudioRecorderButton extends Button {
+
+    public AudioRecorderButton(Context context) {
+        super(context);
+    }
+
+    public AudioRecorderButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AudioRecorderButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
