cef5fd2bc33d775a50b81a228d27103e08c95840
FengYi Lee <FengYi.li@hotmail.com>
2016-04-25 00:48:55 +0800

    ADD 用户端建议和投诉页面

--- app/src/main/java/com/yuan/skeleton/ui/fragment/UserProposalFragment.java
+++ app/src/main/java/com/yuan/skeleton/ui/fragment/UserProposalFragment.java
@@ -2,6 +2,8 @@ package com.yuan.skeleton.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
