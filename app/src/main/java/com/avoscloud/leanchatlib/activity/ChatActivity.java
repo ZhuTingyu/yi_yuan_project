51bdc3eef660c1893e2e890f88fc41b70e65dd01
Wei Zhou <alsor.zhou@gmail.com>
2016-06-07 23:42:03 +0800

    Update : 图片资源更新

--- app/src/main/java/com/avoscloud/leanchatlib/activity/ChatActivity.java
+++ app/src/main/java/com/avoscloud/leanchatlib/activity/ChatActivity.java
@@ -1,7 +1,6 @@
 package com.avoscloud.leanchatlib.activity;
 
 import android.annotation.TargetApi;
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
--- app/src/main/java/com/yuan/house/common/Constants.java
+++ app/src/main/java/com/yuan/house/common/Constants.java
@@ -5,48 +5,35 @@ package com.yuan.house.common;
  * Created by Alsor Zhou on 2/13/15.
  */
 public class Constants {
+    public static final String kServiceCheckUpdate = "checkupdate";
+    public static final String kServiceLogin = "login";
     public static String kApplicationId = "com.mowelltech.threec";
-
     public static String kWebServiceAPIEndpoint = "http://house.ieyuan.com/api";
-
     public static String kWebServiceFileUpload = kWebServiceAPIEndpoint + "/file-upload/feedback";
     public static String kWebServiceUploadCommon = kWebServiceAPIEndpoint + "/file-upload/common";
     public static String kWebServiceSendFeedback = kWebServiceAPIEndpoint + "/feedback";
     public static String kWebServiceSwitchable = kWebServiceAPIEndpoint + "/house/switchable/";
-
     // Production Server
     public static String kPrefsFirstLaunch = "kPrefsFirstLaunch";
-
     public static int kSplashTimeInterval = 20;
-
     public static String kLocalWebHTMLFolder = "file:///android_asset/html";
-
     public static String kLocalWebHTMLPageFolder = "file:///android_asset/3c_html";
-
-//TODO: not working with query parameters..
+    //TODO: not working with query parameters..
     public static String kWebPageEntry = "nearby.html";
     public static String kWebPageLogin = "login.html";
-
     // Web package version
     public static String kWebPackageVersionCached = "kWebPackageVersionCached";
     public static String kWebLaunchImageVersionCached = "kWebLaunchImageVersionCached";
-
+//    public static String kApplicationLaunchImageVersion = "0.0.1";
     public static String kWebPackageExtracted = "kWebPackageExtracted";
-
     // Native app version
     public static String kApplicationPackageVersion = "0.0.1";
-//    public static String kApplicationLaunchImageVersion = "0.0.1";
-
     /**
      * LeanCloud Application Settings.
      * https://leancloud.cn/app.html?appid=9hk99pr7gknwj83tdmfbbccqar1x2myge00ulspafnpcbab8#/key
      */
     public static String kAVApplicationId = "IwzlUusBdjf4bEGlypaqNRIx-gzGzoHsz";
     public static String kAVClientKey = "KLhHUoBqw5G1uMjrTsEqbaVR";
-
-    public static final String kServiceCheckUpdate = "checkupdate";
-    public static final String kServiceLogin = "login";
-
     public static String kCheckTypeHtml = "KidsParentHtmlPackage";
     public static String kCheckTypeLaunchImage = "KidsParentLaunchImage";
 
@@ -77,6 +64,17 @@ public class Constants {
     public static String kFragmentTagBBS = "kFragmentTagBBS";
     public static String kFragmentTagChat = "kFragmentTagChat";
     public static String kWebPageUserBBS = "user_bbs.html";
-    public static String kWebPageAgentBBS = "agency_bbs.html";
+    public static String kWebPageAgencyBBS = "agency_bbs.html";
     public static String kWebPageUserIndex = "user_index.html";
+    public static String kWebPageAgencyIndex = "agency_index.html";
+
+
+    /**
+     * 用于从 Java 代码中调用的 Javascript 方法名
+     */
+    public static String kJavascriptFnOnRightItemClick = "onRightItemClick";
+    public static String kWebPageAgencyMessage = "agency_message.html";
+    public static String kWebpageUserMessage = "user_message.html";
+    public static String kWebpageUserCenter = "user_center.html";
+    public static String kWebpageAgencyCenter = "agency_center.html";
 }
