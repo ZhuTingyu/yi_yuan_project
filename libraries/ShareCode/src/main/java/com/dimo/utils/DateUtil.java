b4f94f726d7a2a1b70d13d3c324daf843563128f
Wei Zhou <alsor.zhou@gmail.com>
2016-06-22 15:59:00 +0800

    Add : 新增 date 工具类

---
+++ libraries/ShareCode/src/main/java/com/dimo/utils/DateUtil.java
@@ -0,0 +1,149 @@
+package com.dimo.utils;
+
+import android.annotation.SuppressLint;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by Alsor Zhou on 16/4/21.
+ */
+public class DateUtil extends android.text.format.DateUtils{
+    // http://stackoverflow.com/a/11413580
+    public static List<Date> getDates(String dateString1, String dateString2) {
+        ArrayList<Date> dates = new ArrayList<>();
+        DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date1 = null;
+        Date date2 = null;
+
+        try {
+            date1 = df1.parse(dateString1);
+            date2 = df1.parse(dateString2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return getDates(date1, date2);
+    }
+
+    public static List<Date> getDates(Date date1, Date date2) {
+        ArrayList<Date> dates = new ArrayList<>();
+
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+
+        while (!cal1.after(cal2)) {
+            dates.add(cal1.getTime());
+            cal1.add(Calendar.DATE, 1);
+        }
+        return dates;
+    }
+
+    /**
+     * 判断给定字符串时间是否为今日
+     * @param date
+     * @return boolean
+     */
+    public static boolean isToday(Date date){
+        boolean b = false;
+        Date time = date;
+        Date today = new Date();
+        if(time != null){
+            String nowDate = dateFormater2.get().format(today);
+            String timeDate = dateFormater2.get().format(time);
+            if(nowDate.equals(timeDate)){
+                b = true;
+            }
+        }
+        return b;
+    }
+
+    /**
+     * 将字符串转位日期类型
+     * @param sdate
+     * @return
+     */
+    public static Date toDate(String sdate) {
+        try {
+            return dateFormater.get().parse(sdate);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    private final static ThreadLocal<SimpleDateFormat> dateFormater = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    private final static ThreadLocal<SimpleDateFormat> dateFormater2 = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd");
+        }
+    };
+
+    /**
+     * 将yyyy-MM-dd HH:mm:ss字符串转换成日期<br/>
+     *
+     * @param dateStr    时间字符串
+     * @param dataFormat 当前时间字符串的格式。
+     * @return Date 日期 ,转换异常时返回null。
+     */
+    public static Date parseDate(String dateStr, String dataFormat) {
+        try {
+            @SuppressLint("SimpleDateFormat")
+            SimpleDateFormat dateFormat = new SimpleDateFormat(dataFormat);
+            Date date = dateFormat.parse(dateStr);
+            return new Date(date.getTime());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 将yyyy-MM-dd HH:mm:ss字符串转换成日期<br/>
+     *
+     * @param dateStr yyyy-MM-dd HH:mm:ss字符串
+     * @return Date 日期 ,转换异常时返回null。
+     */
+    public static Date parseDate(String dateStr) {
+        return parseDate(dateStr, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    /**
+     * 将指定的日期转换为一定格式的字符串
+     *
+     * @param date
+     * @param format
+     * @return
+     */
+    public static String toDateString(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.CHINA);
+        return sdf.format(date);
+    }
+
+    /**
+     * 将当前的日期转换为一定格式的字符串
+     *
+     * @param format
+     * @return
+     */
+    public static String toDateString(String format) {
+        return toDateString(Calendar.getInstance(Locale.CHINA).getTime(), format);
+    }
+
+}
