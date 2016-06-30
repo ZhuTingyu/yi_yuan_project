26d2c80f6b7e6fd0688172ae3de98574f0c36c41
xianfu xia <xianfu.xia@monstar-lab-cn.com>
2016-06-30 16:28:58 +0800

    update : 修改投诉／建议的历史纪录显示（文字部分）

---
+++ app/src/main/java/com/yuan/house/bean/ProposalInfo.java
@@ -0,0 +1,16 @@
+package com.yuan.house.bean;
+
+/**
+ * Created by monst on 16/6/29.
+ */
+public class ProposalInfo {
+    public String user_id;
+    public int msg_type;
+    public String content;
+    public int type;
+    public int complain_agency_id;
+    public int category;
+    public int duration;
+    public String created_at;
+    public String updated_at;
+}
