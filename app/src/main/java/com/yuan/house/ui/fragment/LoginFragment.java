d6015f68f973b296853dec0d6f1fe5a2f4ce448a
Tingyu Zhu <598511626@qq.com>
2016-07-21 22:47:27 +0800

    登录界面显示city+district

--- app/src/main/java/com/yuan/house/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/yuan/house/ui/fragment/LoginFragment.java
@@ -2,20 +2,29 @@ package com.yuan.house.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.yuan.house.application.DMApplication;
 import com.yuan.house.application.Injector;
 import com.yuan.house.common.Constants;
 import com.yuan.house.R;
+import com.yuan.house.event.LocationEvent;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by KevinLee on 2016/4/22.
  */
 public class LoginFragment extends WebViewBaseFragment {
+
+    @BindView(R.id.fragment_login_address)
+    TextView address;
 
     public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
@@ -41,5 +50,23 @@ public class LoginFragment extends WebViewBaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        BDLocation location = DMApplication.getInstance().getLastActivatedLocation();
+        if (location != null) {
+            address.setText(location.getCity()+location.getDistrict());
+        }
     }
+
+    public void onEvent(LocationEvent event) {
+        if (event.getEventType() == LocationEvent.LocationEventEnum.UPDATED) {
+            BDLocation location = event.getHolder();
+
+            String street = location.getCity()+location.getDistrict();
+            if (TextUtils.isEmpty(street)) {
+                street = getString(R.string.get_location);
+            }
+
+            address.setText(street);
+        }
+    }
+
 }
