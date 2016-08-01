effb32f9c5ad58549a64fb7613970b6f3748b0fc
Wei Zhou <alsor.zhou@gmail.com>
2016-08-01 23:59:50 +0800

    Add : view helper function to support fast disable view and its childs

--- libraries/ShareCode/src/main/java/com/dimo/helper/ViewHelper.java
+++ libraries/ShareCode/src/main/java/com/dimo/helper/ViewHelper.java
@@ -28,4 +28,14 @@ public class ViewHelper {
         return views;
     }
 
+    public static void setViewAndChildrenEnabled(View view, boolean enabled) {
+        view.setEnabled(enabled);
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                View child = viewGroup.getChildAt(i);
+                setViewAndChildrenEnabled(child, enabled);
+            }
+        }
+    }
 }
