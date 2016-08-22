5e2e0164563296b3063b838c2814bcd6b6cbb1e0
Wei Zhou <alsor.zhou@gmail.com>
2016-08-22 17:54:52 +0800

    Fix : 在聊天界面被踢下线无法及时登录，需要返回之后才可以的问题

--- app/src/main/java/com/yuan/house/activities/MainActivity.java
+++ app/src/main/java/com/yuan/house/activities/MainActivity.java
@@ -215,11 +215,9 @@ public class MainActivity extends WebViewBasedActivity implements WebViewFragmen
     public void onEvent(AuthEvent event) {
         if (event.getEventType() == AuthEvent.AuthEventEnum.LOGOUT) {
             // 注销已完成, 重新显示登录界面
-            if (getWebViewFragment().getClass() != LoginFragment.class) {
-                // API 11 +
-                recreate();
-            }
-        }
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);        }
     }
 
     @Override
